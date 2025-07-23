17fc8d7dd52e6095247ef45e16a4fae57a17f1eb
srjonjoy <ghoshsrinjoy2@gmail.com>
2025-07-23 10:22:04 +0530

    creating register page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from "react-router-dom";
 import {
   Card,
@@ -9,31 +9,79 @@ import {
   CardHeader,
   CardTitle,
 } from "../components/ui/card";
+import { FaRegEye } from "react-icons/fa";
+import { FaRegEyeSlash } from "react-icons/fa";
 import { Input } from "../components/ui/input";
 import { Label } from "../components/ui/label";
 import { Button } from "../components/ui/button";
 import logo from "../assets/Read.png"
 
 const Login = () => {
+  const [show,setShow]=useState(false)
+  const navigate = useNavigate()
   return (
     <div className="min-h-screen flex items-start justify-center pt-20 px-4 bg-gray-50">
       <Card className="w-full max-w-sm shadow-lg">
         <CardHeader>
           {/* logo */}
-          <img
-            src={logo}
-            alt="ReadWrite Logo"
-            className="w-16 h-16 object-contain"
-          />
-          {/* heading */}
-          <h1 className="text-xl font-semibold text-center text-gray-800 mb-2">
-            Welcome to Read & Write
-          </h1>
+          <div className='logo' onClick={() => navigate('/')}>
+            <img
+              src={logo}
+              alt="ReadWrite Logo"
+              className="w-16 h-16 object-contain"
+            />
+            {/* heading */}
+            <h1 className="text-xl font-semibold text-center text-gray-800 mb-2">
+              Welcome to Read & Write
+            </h1>
+          </div>
           <CardTitle className="text-center">enter your email and password</CardTitle>
         </CardHeader>
         <CardContent>
           {/* Your form elements go here */}
+          {/* emal */}
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+
+
+          </div>
+          {/* pass */}
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="password">Password</Label>
+              <a
+                href="#"
+                className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+              >
+                Forgot your password?
+              </a>
+            </div>
+            <div className="relative">
+              <Input id="password" type={show?"text":"password"} required className="pr-10" />
+
+              {/* Eye icon */}
+              {!show &&<FaRegEye className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer" onClick={()=>setShow(prev=>!prev)} />}
+              {show &&<FaRegEyeSlash className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer" onClick={()=>setShow(prev=>!prev)} />}
+            </div>
+          </div>
+          {/* login button */}
+          <Button type="submit" className="w-full" >
+            login
+          </Button>
         </CardContent>
+        <div className="px-6 pb-4">
+          <Button variant="link" className="w-full" onClick={() => navigate("/singup")}>
+            Dont have an account? Register first
+          </Button>
+        </div>
 
       </Card>
     </div>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -13,26 +13,30 @@ import { Input } from "../components/ui/input";
 import { Label } from "../components/ui/label";
 import { Button } from "../components/ui/button";
 import logo from "../assets/Read.png"
+import google from "../assets/google.jpg"
 
 
 
 export const Register = () => {
-  const navigate=useNavigate()
+  const navigate = useNavigate()
   return (
-    
+
     <div className="min-h-screen flex items-start justify-center pt-20 px-4 bg-gray-50">
       <Card className="w-full max-w-sm shadow-lg">
         <CardHeader>
-        {/* logo */}
-        <img
-            src={logo} 
-            alt="ReadWrite Logo"
-            className="w-16 h-16 object-contain"
-          />
-        {/* heading */}
-          <h1 className="text-xl font-semibold text-center text-gray-800 mb-2">
-            Welcome to Read & Write
-          </h1>
+          {/* logo */}
+          <div className='logo' onClick={() => navigate('/')}>
+            <img
+              src={logo}
+              alt="ReadWrite Logo"
+              className="w-16 h-16 object-contain"
+            />
+            {/* heading */}
+            <h1 className="text-xl font-semibold text-center text-gray-800 mb-2">
+              Welcome to Read & Write
+            </h1>
+
+          </div>
           <CardTitle className="text-center">Register your details</CardTitle>
           <CardDescription className="text-center">
             Enter your details to sign up
@@ -41,6 +45,82 @@ export const Register = () => {
 
         <CardContent>
           {/* Your form elements go here */}
+          {/* firstname */}
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="firstname">Firstname</Label>
+                <Input
+                  id="firstname"
+                  type="text"
+                  placeholder="srinjoy"
+                  required
+                />
+              </div>
+            </div>
+            {/* lastname */}
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="lastname">Lastnaame</Label>
+                <Input
+                  id="lastname"
+                  type="text"
+                  placeholder="Goswami"
+                  required
+                />
+              </div>
+              {/* emal */}
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    required
+                  />
+                </div>
+
+
+              </div>
+              {/* pass */}
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              {/* button submit */}
+               <Button type="submit" className="w-full" >
+               Create Account
+            </Button>
+
+            </div>
+            {/* login with google */}
+            <Card className='googlelogin  shadow-lg flex justify-center align-bottom w-full h-20'>
+              <div className="google flex items-center justify-center gap-4 mt-6">
+                <img
+                  src={google}
+                  alt="ReadWrite Logo"
+                  className="w-16 h-10 object-contain"
+                />
+                {/* heading */}
+                <p className=" font-semibold text-gray-800">
+                  Login using Google
+                </p>
+              </div>
+
+
+            </Card>
+
+
+          </form>
         </CardContent>
 
         <div className="px-6 pb-4">
@@ -48,6 +128,7 @@ export const Register = () => {
             Already have an account? Login
           </Button>
         </div>
+
       </Card>
     </div>
   )
